b58ddb764bae4261e64da55bc50ada4b2838a7a6
Carlos De Vera <cedevera02@gmail.com>
2021-02-16 10:17:42 -0600

    Just testing git on my host. Nothing big

---
+++ Ship.js
@@ -0,0 +1,24 @@
+class Ship{
+    constructor(size) {
+        let m_size = size;
+    }
+    /**
+     * @pre m_size must have valid value
+     * @post returns m_size
+     */
+    getSize() {
+        return m_size
+    }
+    /**
+     * @pre gameboard must be set up
+     * @post positions the ship on gameboard
+     * @param startPos starting position of ship
+     * @param endPos ending position of ship
+     */
+    setPosition(startPos, endPos) {
+
+    }
+    hit() {
+
+    }
+}
